3d0eaebd6aa21e0639a1d7a61b3df9069258a7a4
Yiding He <yhe@truxnow.com>
2020-02-03 16:19:36 -0500

    Added ProductCategory and repo.

---
+++ sell_server/src/test/java/com/imooc/sell/repository/ProductCategoryRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.imooc.sell.repository;
+
+import com.imooc.sell.dataobject.ProductCategory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryRepositoryTest {
+
+    @Autowired
+    private ProductCategoryRepository repository;
+
+    @Test
+    public void findOneTest() {
+        ProductCategory productCategory = repository.findById(1).get();
+        System.out.println(productCategory);
+    }
+
+    @Test
+    @Transactional
+    public void saveTest() {
+        ProductCategory productCategory = new ProductCategory("男生最爱", 4);
+        ProductCategory result = repository.save(productCategory);
+//        Assert.assertNotEquals(null, result);
+    }
+}
